dcb75bdeb2bcd6b45cf7f8813bbbee4a1814cafc
Barthelemy <barthelemy.ehui@gmail.com>
2018-09-20 16:59:45 +0200

    create file controller

---
+++ app/Http/Controllers/Account/FileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\File;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FileController extends Controller
+{
+    public function create(File $file)
+    {
+        if(!$file->exists) {
+            $file = $this->createAndReturnSkeletonFile();
+    
+            return redirect()->route('account.files.create', $file);
+        }
+        
+        return 'account created';
+    }
+    
+    private function createAndReturnSkeletonFile()
+    {
+        return auth()->user()->files()->create([
+            'title' => 'Untitle',
+            'overview' => 'None',
+            'overview_short' => 'None',
+            'price' => 0,
+            'finished' => false
+        ]);
+    }
+}
