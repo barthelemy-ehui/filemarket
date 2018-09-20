546c8bca2b36a5f47c27b8906d5de9b429281b74
Barthelemy <barthelemy.ehui@gmail.com>
2018-09-20 17:16:59 +0200

    call the authorize inside File create method

--- app/Http/Controllers/Account/FileController.php
+++ app/Http/Controllers/Account/FileController.php
@@ -16,6 +16,8 @@ class FileController extends Controller
             return redirect()->route('account.files.create', $file);
         }
         
+        $this->authorize('touch');
+        
         return 'account created';
     }
     
