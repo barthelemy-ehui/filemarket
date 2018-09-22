1187b3cbc0628ca25e258cbe15801faee90ccfde
Barthelemy <barthelemy.ehui@gmail.com>
2018-09-22 17:50:38 +0200

    add edit view

---
+++ resources/views/files/partials/_file.blade.php
@@ -0,0 +1,12 @@
+<article class="media">
+    <div class="media-content">
+        <div class="content">
+            <strong>
+                <a href="#">{{ $file->title }}</a>
+            </strong>
+            <br/>
+            {{ $file->overview_short }}
+        </div>
+        {{ $links }}
+    </div>
+</article>
