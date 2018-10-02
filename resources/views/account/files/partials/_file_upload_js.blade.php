d910720040864584a4e7dd39d10566d7c4b88f1f
Barthelemy <barthelemy.ehui@gmail.com>
2018-10-02 11:55:24 +0200

    delete file when removed, fill delete column

--- resources/views/account/files/partials/_file_upload_js.blade.php
+++ resources/views/account/files/partials/_file_upload_js.blade.php
@@ -9,5 +9,32 @@
             }
 
         });
+
+
+        @foreach($file->uploads as $upload)
+            drop.emit('addedfile', {
+                id: '{{ $upload->id }}',
+                name: '{{ $upload->filename }}',
+                size: '{{ $upload->size }}',
+            });
+        @endforeach
+
+
+        drop.on('success', function(file, response) {
+            file.id = response.id;
+        });
+
+        drop.on('removedfile', function(file) {
+
+            axios.delete('/{{ $file->identifier }}/upload/' + file.id).catch(function (error) {
+                drop.emit('addedfile', {
+                    id: file.id,
+                    name: file.name,
+                    size: file.size,
+                });
+            });
+
+        });
     });
+
 </script>
