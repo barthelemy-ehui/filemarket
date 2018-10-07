bef8af5a393d4a243b89fac6eab47569241bb7c6
Barthelemy <barthelemy.ehui@gmail.com>
2018-10-07 07:44:43 +0200

    add admin page area

---
+++ resources/views/admin/layouts/partials/_stats.blade.php
@@ -0,0 +1,30 @@
+<section class="hero is-primary">
+    <div class="hero-body">
+        <div class="level is-mobile">
+            <div class="level-item has-text-centered">
+                <div>
+                    <div class="heading">Total files</div>
+                    <div class="title">0</div>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <div class="heading">Total Sales</div>
+                    <div class="title">0</div>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <div class="heading">Commission this month</div>
+                    <div class="title">0€</div>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <div class="heading">Lifetime commission</div>
+                    <div class="title">0€</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
