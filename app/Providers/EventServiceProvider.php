84a1c009d2bf219fe231314dfbf7c4257c313661
Barthelemy <barthelemy.ehui@gmail.com>
2018-10-19 21:22:08 +0200

    send email to buyer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\Checkout\SaleCreated;
+use App\Listeners\Checkout\SendEmailToBuyer;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        SaleCreated::class => [
+            SendEmailToBuyer::class
+        ]
     ];
 
     /**
